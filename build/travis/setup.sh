f5e45af08c94cd1910a39522c28e922c66d5f5c9
epoxa <epoxa@vvproject.ru>
2017-01-14 20:56:54 +0700

    MOD: Set up for travis-ci

--- build/travis/setup.sh
+++ build/travis/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo ls -l /usr/sbin/ph*
+php -v
+php -i
 sudo apt-get update
 sudo apt-get -y install nginx
 cat build/travis/etc/nginx/travis.nginx.conf | sed -e "s,\$DOCROOT,`pwd`," | sudo tee /etc/nginx/nginx.conf
