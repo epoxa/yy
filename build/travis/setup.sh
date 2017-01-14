205c65b0b8d5d56f749afb7bfbbfec8443c6b854
epoxa <epoxa@vvproject.ru>
2017-01-14 18:36:06 +0700

    MOD: Set up for travis-ci

--- build/travis/setup.sh
+++ build/travis/setup.sh
@@ -2,7 +2,6 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 cat build/travis/etc/nginx/travis.nginx.conf | sed -e "s,\$DOCROOT,`pwd`," | sudo tee /etc/nginx/nginx.conf
-cat /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar --quiet
-java -jar selenium-server-standalone-2.47.1.jar &
+
