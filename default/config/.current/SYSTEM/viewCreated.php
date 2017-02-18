fa65410463ccc5a7d32f28a70f6804d8508e5a8b
epoxa <epoxa@vvproject.ru>
2017-02-19 00:04:57 +0700

    ADD: Translation demo

--- default/config/.current/SYSTEM/viewCreated.php
+++ default/config/.current/SYSTEM/viewCreated.php
@@ -1,5 +1,6 @@
 <?php
 
+use YY\Demo\Translation\Agent;
 use YY\System\YY;
 use YY\Demo\Demo;
 
@@ -17,3 +18,10 @@ if (isset(YY::$ME['curator'])) {
 }
 
 YY::$CURRENT_VIEW['ROBOT'] = $curator;
+
+if (isset(YY::$ME['selectedLanguage'])) {
+	YY::$CURRENT_VIEW['LANGUAGE'] = YY::$ME['selectedLanguage'];
+}
+if (isset(YY::$ME['translateMode'])) {
+	YY::$CURRENT_VIEW['TRANSLATOR'] = new Agent();
+}
